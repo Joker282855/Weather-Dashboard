b56a8faf1e1006528adfffb47dd9fb22b82562c3
unknown <jtjackson325@gmail.com>
2021-07-24 16:07:16 -0600

    Set up the inital part fo the javascpirt funciton that logs the array when a city is searched

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,9 @@
-var getCityInformation = function(search) {
+var cityFormEl = document.querySelector("#city-box");
+var cityInputEl = document.querySelector("#city-search");
+
+var getCityInformation = function() {
   // formatting the api
-  var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=22f77c72887272eb5c62d174a8e491b8";
+  var cityUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=43.93&lon=-74.56&exclude=minutely,alerts&appid=22f77c72887272eb5c62d174a8e491b8";
 
   // make the fetch request to the api
   fetch(cityUrl).then(function(response) {
@@ -10,4 +13,17 @@ var getCityInformation = function(search) {
   })
 };
 
-getCityInformation("Manhattan");
+var citySubmitHandler = function(event) {
+    var cityName = cityInputEl.value.trim();
+
+    if (cityName) {
+        getCityInformation(cityName);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter the name of an American City");
+    }
+};
+
+getCityInformation();
+
+cityFormEl.addEventListener("submit", citySubmitHandler);
