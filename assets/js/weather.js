a8ecf7efb25c6b85790eac08218ead4c5df7abb5
unknown <jtjackson325@gmail.com>
2021-07-25 14:57:48 -0600

    was able to get the city we searched for to display on the webpage

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,19 +1,22 @@
+var citySearchEl = document.querySelector("#search-history");
 var cityFormEl = document.querySelector("#city-box");
 var cityInputEl = document.querySelector("#city-search");
 
-var getCityInformation = function() {
+var getCityInformation = function(city) {
   // formatting the api
-  var cityUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=43.93&lon=-74.56&exclude=minutely,alerts&appid=22f77c72887272eb5c62d174a8e491b8";
+  var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=22f77c72887272eb5c62d174a8e491b8";
 
   // make the fetch request to the api
   fetch(cityUrl).then(function(response) {
       response.json().then(function(data) {
-          console.log(data);
+          displayCityName(data, city);
       })
   })
 };
 
 var citySubmitHandler = function(event) {
+    event.preventDefault();
+    
     var cityName = cityInputEl.value.trim();
 
     if (cityName) {
@@ -22,8 +25,37 @@ var citySubmitHandler = function(event) {
     } else {
         alert("Please enter the name of an American City");
     }
+    console.log(event);
 };
 
-getCityInformation();
+var displayCityName = function(city, searchTerm) {
+    console.log(city);
+    console.log(searchTerm);
+
+    // clear old city search
+    citySearchEl.textContent = "";
+
+    // loop over the city searches
+    for (var i = 0; i < city.name.length; i++) {
+        // format city's name
+        var cityName = city.name;
+        console.log(cityName);
+
+        // create a container for each of the buttons
+        var cityEl = document.createElement("div");
+        cityEl.classList = "list-item flex-row justify-space-between align-center"
+
+        // create the button for the recent search
+        var buttonEl = document.createElement("button");
+        buttonEl.textContent = cityName;
+        buttonEl.classList = "bg-secondary"
+
+        // append the button element to the container
+        cityEl.appendChild(buttonEl);
+
+        // append the div container to its parent container
+        citySearchEl.appendChild(cityEl);
+    }
+}
 
 cityFormEl.addEventListener("submit", citySubmitHandler);
